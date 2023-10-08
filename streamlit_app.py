b6142b0023ac3b0aa8f052fd977a598d70fda338
Lenar9108 <69675412+Lenar9108@users.noreply.github.com>
2023-10-08 18:26:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as ex
 
 st.set_page_config(
     page_title="Dash App",
@@ -24,19 +25,22 @@ with st.sidebar:
         country_list,
         country_list[:2])
 
-
     
 st.subheader('Ежедневная выручка по регионам')
 
 df_revenue = df[['rent_date', 'country_name','rent_cost_per_day']].groupby(['rent_date', 'country_name']).sum().reset_index().copy()
 df_revenue = df_revenue.rename(columns={'rent_date':'Дата', 'country_name':'Страна','rent_cost_per_day':'Дневная выручка'})
-st.line_chart(df_revenue[df_revenue['Страна'].isin(options)], x="Дата", y="Дневная выручка", color="Страна")
-
+    
+fig = ex.line(df_revenue[df_revenue['Страна'].isin(options)], 
+          x="Дата", y="Дневная выручка", color="Страна", line_group="Страна", hover_name="Страна",
+    line_shape="spline", render_mode="svg")
+st.write(fig)
 
 df_users = df[['rent_date', 'rent_id', 'country_name']].groupby(['rent_date', 'country_name']).count().reset_index().copy()
 df_users = df_users.rename(columns={'rent_date':'Дата', 'country_name':'Страна', 'rent_id': 'Кол-во пользователей'})
 
 st.subheader('Кол-во активных пользователей')
-st.bar_chart(df_users[df_users['Страна'].isin(options)], x='Дата', y='Кол-во пользователей' )
 
+fig = ex.bar(df_users[df_users['Страна'].isin(options)], x="Дата", y="Кол-во пользователей", barmode="group")
+st.write(fig)
 
